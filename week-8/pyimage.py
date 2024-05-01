506559a54c92fa53836160b894338041271bf710
Alisha538 <159217641+Alisha538@users.noreply.github.com>
2024-05-01 06:19:40 -0700

    Update and rename imagee.py to pyimage.py

--- week-8/pyimage.py
+++ week-8/pyimage.py
@@ -1,11 +1,11 @@
 from PIL import image
 image_path=''
-image=image.open('/home/rguktrkvalley/Desktop/cl/week8/1701322810442.png')
+image=image.open("C:\Users\farra\OneDrive\Documents\CL\week-8\Alisha.jpg")
 x,y=100,100
 rgb=image.getpixel((x,y))
 print(f'RGB values at position ({x},{y}): {rgb}')
 new_rgb=(255,0,0)
 image.putpixel((x,y),new_rgb)
-output_path='/home/rguktrkvalley/Desktop/cl/week8/1701322810442.png'
+output_path="C:\Users\farra\OneDrive\Documents\CL\week-8\Alisha.jpg"
 image.save(output_path)
 image.close()

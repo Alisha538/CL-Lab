f56e5e14179acdab512356209c79852e8336ec50
Alisha538 <159217641+Alisha538@users.noreply.github.com>
2024-05-01 06:03:33 -0700

    Update audio.m

--- week-7/audio.m
+++ week-7/audio.m
@@ -1,7 +1,7 @@
 clc
 clear all
 close all
-file_path='/home/rguktrkvalley/Desktop/cl/week7/sample-3s.wav'
+file_path='C:\Users\farra\OneDrive\Documents\CL\week-7/sample-3s.wav'
 [signal,sample_rate]=audioread(file_path)
 duration=length(signal)/sample_rate;
 time=(0:length(signal)-1)/sample_rate;

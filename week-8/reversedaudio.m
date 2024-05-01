51091917c96605c94b09ee40405d00f79028d27a
Alisha538 <159217641+Alisha538@users.noreply.github.com>
2024-05-01 06:18:04 -0700

    Update and rename audioorev.m to reversedaudio.m

--- week-8/reversedaudio.m
+++ week-8/reversedaudio.m
@@ -1,5 +1,5 @@
-inputfile='/home/rguktrkvalley/Downloads/cl/week8/sample-3s.wav'
-outputfile='/home/rguktrkvalley/Downloads/cl/week8/sample-3s.wav'
+inputfile="C:\Users\farra\OneDrive\Documents\CL\week-8\sample-3s.wav"
+outputfile="C:\Users\farra\OneDrive\Documents\CL\week-8\sample-3s.wav"
 [y,Fs]=audioread(inputfile);
 reversedData=flipud(y);
 audiowrite(outputfile,reversedData,Fs);
